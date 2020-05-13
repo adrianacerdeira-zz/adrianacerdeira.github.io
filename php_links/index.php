6d073814b6abbe4621fe80d245790afad301168d
Adriana Cerdeira <adrianacerdeira@users.noreply.github.com>
2020-05-13 16:31:10 -0300

    Teste PHP
    Testando o PHP do Pages

---
+++ php_links/index.php
@@ -0,0 +1,40 @@
+<?php 
+include '../includes/header.php';
+?>
+    <section id="adriana" class="recursos">
+        <h2>Minhas Novidades e Canais</h2>
+        <section class="recursos_internos">
+            <h3>Meu site </h3>
+            <dl class="recursos_internos">
+
+                <dt>Newsletter - cadastre-se para receber as novidades</dt>
+                <dd><a href="https://www.subscribepage.com/Cbbrnewsletter" target="_blank"
+                       alt="Link para se cadastrar no Newsletter">
+                    https://www.subscribepage.com/Cbbrnewsletter</a></dd>
+
+                <dt>Computer Buddy BR</dt>
+                <dd><a href="http://adrianacerdeira.info/" target="_blank" alt="Link para meu site">
+                    http://adrianacerdeira.info/</a></dd>
+
+                <dt>Perfil Udemy</dt>
+                <dd><a href="https://www.udemy.com/user/adrianacerdeira" target="_blank"
+                       alt="Link para meu perfil Udemy">https://www.udemy.com/user/adrianacerdeira</a></dd>
+                <dt>Cupons de Desconto</dt>
+                <dd>
+                    <p>A nova política de cupons da Udemy não permite mais criar cupons vitalícios, eles agora têm
+                        validade de no máximo 30 dias. Por isso, minha estratégia de cupons foi alterada. Para ficar por dentro e continuar recebendo os cupons com as promoções de 5 dias com melhor preço, por favor <a href="https://www.subscribepage.com/Cbbrnewsletter" target="_blank"
+                       alt="Link para se cadastrar no Newsletter">inscreva-se no Newsletter</a>.</p>
+
+                    <p>Além, disso, todo mês eu criarei um cupom com o nome do mês e os últimos dois dígitos do ano, por exemplo:
+                        <strong>JUNHO20</strong>. Então você poderá colocar o cupom dessa maneira dependendo do mês e
+                        ano da compra.</p>
+
+                </dd>
+
+
+            </dl>
+        </section>
+
+<?php 
+include '../includes/footer.php';
+?>
